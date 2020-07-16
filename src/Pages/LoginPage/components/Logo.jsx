4ea9a108b7de7031fbd79d082d105e464a298a9b
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-07-15 21:32:14 -0400

    created logo component

---
+++ src/Pages/LoginPage/components/Logo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const logoStyles = {
+     fontFamily: "'Asap', sans-serif"
+}
+
+const Logo = () => {
+     return (
+          <>
+               <h2>facebook</h2>
+          </>
+     )
+}
+
+export default Logo
