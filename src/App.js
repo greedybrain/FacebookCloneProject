1f50e287d7e4123c1c4b752bd7779303ae53f79e
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-07-15 21:31:45 -0400

    removed import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import HeaderIfLoggedOut from './Pages/LoginPage/containers/HeaderIfLoggedOut';
 
 function App() {
   return (
     <div className="App">
-      
+      <HeaderIfLoggedOut />
     </div>
   );
 }
